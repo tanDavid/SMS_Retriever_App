b6605ca833d938996cfe4d73e1e2dbe6287f065c
Ju Long <19013345@myrp.edu.sg>
2021-05-25 14:51:01 +0800

    updated on the first fragment

--- app/src/main/java/com/myapplicationdev/android/smsretrieverapp/FirstFragment.java
+++ app/src/main/java/com/myapplicationdev/android/smsretrieverapp/FirstFragment.java
@@ -35,47 +35,48 @@ public class FirstFragment extends Fragment {
         btnRetrieve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//Create all messages uri
+                Uri uri = Uri.parse("content://sms");
 
-                if (!tvSMS.getText().toString().isEmpty()){
-                    //Create all messages uri
-                    Uri uri = Uri.parse("content://sms");
+                //the columns we want
+                //date is when the message took place
+                // address is the no. of the other party
+                // body is the message content
+                // type 1 is received, type 2 is sent
+                String[] reqCols = new String[]{"date", "address", "body", "type"};
 
-                    //the columns we want
-                    //date is when the message took place
-                    // address is the no. of the other party
-                    // body is the message content
-                    // type 1 is received, type 2 is sent
-                    String[] reqCols = new String[]{"date", "address", "body", "type"};
+                //get content resolver obj form which to query the content provider
+                Context applicationContext = MainActivity.getContextOfApplication();
+                ContentResolver cr = applicationContext.getContentResolver();
 
-                    //get content resolver obj form which to query the content provider
-                    Context applicationContext = MainActivity.getContextOfApplication();
-                    ContentResolver cr = applicationContext.getContentResolver();
+                String filter = null;
+                String[] filterArgs = null;
 
-                    String filter="body LIKE ? AND body LIKE ?";
-                    String[] filterArgs = {"%late%", "%min%"};
-
-                    //fetch sms messafe frm built in content provider
-                    Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
-                    String smsBody = "";
-
-                    if(cursor.moveToFirst()){
-                        do{
-                            long dateInMillis = cursor.getLong(0);
-                            String date = (String) DateFormat.format("dd MMM yyyy h:mm:ss aa", dateInMillis);
-                            String address = cursor.getString(1);
-                            String body = cursor.getString(2);
-                            String type = cursor.getString(3);
-                            if(type.equalsIgnoreCase("1")){
-                                type = "Inbox";
-                            }else{
-                                type = "Sent:";
-                            }
-                            smsBody += type + " " + address + "\n at " + date + "\n\"" + body + "\"\n\n";
-                        }while (cursor.moveToNext());
-                    }
-                    tvSMS.setText(smsBody);
+                if (!tvSMS.getText().toString().isEmpty()) {
+                    filter = "body LIKE ? AND body LIKE ?";
+                    filterArgs = new String[]{"%" + etNumber.getText().toString() + "%"};
                 }
 
+                //fetch sms messafe frm built in content provider
+                Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+                String smsBody = "";
+
+                if(cursor.moveToFirst()){
+                    do{
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat.format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if(type.equalsIgnoreCase("1")){
+                            type = "Inbox";
+                        }else{
+                            type = "Sent:";
+                        }
+                        smsBody += type + " " + address + "\n at " + date + "\n\"" + body + "\"\n\n";
+                    }while (cursor.moveToNext());
+                }
+                tvSMS.setText(smsBody);
             }
         });
 
